e31831769bf5bb6356ce97c8ab9b8c4a95626c3c
Student10039 <153464754+Student10039@users.noreply.github.com>
2023-12-11 00:46:38 +0100

    final polish
    Im done i swear

--- IDG1100Deliver/generatepage.sh
+++ IDG1100Deliver/generatepage.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
-# Function to generate HTML content for a municipality
+# Function to print paragraph for the municipality page
 generate_html_content() {
     municipality_data="$1"
 
-    # Output HTML content for the provided municipality data
+    # Output the paragraph for the provided municipality
     printf "$municipality_data"
 }
 
 # Check if the query string is empty, indicating an invalid request
 if [ -z "$QUERY_STRING" ]; then
-    # Output a basic error HTML page for a 404 - Page not found error
+    # Output a error page not found error
     echo "Content-type: text/html"
     echo ""
     echo "<html><body>"
     echo "<h1>Error 404 - Page not found</h1>"
     echo "</body></html>"
 else
-    # find the municipality from the query string
+    # find the municipality from the query string 
     municipality=$(echo "$QUERY_STRING" | sed -n 's/^.*municipality=\([^&]*\).*$/\1/p')
 
     if [ -z "$municipality" ]; then
-        # Output a custom error HTML page for a 404 - Page not found error with missing municipality parameter
+        # Output a error page for a not found error with missing parameter
         echo "Content-type: text/html"
         echo ""
         echo "<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><title>The Weather for: "$municipality"</title> <link rel="stylesheet" href="style.css"> </head><body><main>"
         echo "<h1>Error 404 - Page not found</h1>"
         echo "</main><footer><p>Developed for the course IDG1100 by candidate number: 10039</p></footer></body></html>"
     else
-        # Output HTML header and body tags for the weather information page
+        # echo HTML head and body tags for the weather information page
         echo "Content-type: text/html"
         echo ""
         echo "<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><title>The Weather for: "$municipality"</title> <link rel="stylesheet" href="style.css"> </head><body><main>"
 
-        # Read data from the text file and find the matching municipality, prints the paragraph in the matching line:
+        # Read data from the text file and find the matching municipality, prints the paragraph from the matching line:
        municipality_data=$(awk -v municipality="$municipality" -F'</p>' 'BEGIN{RS="<p>"} $0 ~ municipality {print $0"</p>"}' webpage.txt)
 
 
         if [ -z "$municipality_data" ]; then
-            # Output a custom error HTML page for a 404 - Municipality not found error
+            # Output a error page for a Municipality not found
             echo "<h1>Error 404 - Municipality "$municipality" not found</h1>"
         else
-            # Call the function to generate and output HTML content for the specified municipality
+            # Call the function to generate and output Paragraph with data for the specified municipality
             generate_html_content "$municipality_data"
         fi
 
-        # Output HTML footer and closing tags
+        # echo the footer and ending tags of the HTML
         echo "</main><footer><p>Developed for the course IDG1100 by candidate number: 10039</p></footer></body></html>"
     fi
 fi
--- IDG1100Deliver/setup.sh
+++ IDG1100Deliver/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
 #Change this to your folder and change it in the "uodatewebsite.sh" script too
 PROJECT_DIR=/home/eirikfin/Documents/GitHub/IDG1100EXAM
+
 # Making directory for the webpage
 sudo mkdir -p /var/www/ubervaer/public_html
+
 #copy the css file and the webpage script to the webpage folder
 sudo cp style.css /var/www/ubervaer/public_html/style.css
 sudo cp generatepage.sh /var/www/ubervaer/public_html/generatepage.sh
+
 # Add hostname to /etc/hosts
 echo "127.0.0.25 uber.vaer" | sudo tee -a /etc/hosts
 
@@ -13,7 +17,7 @@ echo "127.0.0.25 uber.vaer" | sudo tee -a /etc/hosts
 echo "" > frontpage.txt
 # Loop through each line in the fulldata.txt file and creating the front page links
 while IFS=$'\t' read -r url place population lat latvalue lon lonvalue temp prec humidity weekday date month time; do
-    printf "<p><a href=\"generatepage.sh?municipality=%s\">%s</p>\n" "$place" "$place" >> "$PROJECT_DIR/frontpage.txt"
+    printf "<p><a href=\"generatepage.sh?municipality=%s\">%s</p></a>\n" "$place" "$place" >> "$PROJECT_DIR/frontpage.txt"
 done < "$PROJECT_DIR/fulldata.txt"
 
 webdata=$(cat "$PROJECT_DIR/frontpage.txt")
